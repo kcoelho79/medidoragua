dfca7d8dd77b12fcd7b6e1e547de488ac86c213a
Medidor de Agua <kleber.amg79@gmail.com>
2020-05-12 19:38:25 -0300

    update script-agua.sh

---
+++ script-agua.sh
@@ -0,0 +1,8 @@
+#! /bin/bash     
+PATH=/home/kwmedagua/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+. /home/kwmedagua/venv/bin/activate 
+cd /home/kwmedagua
+python medidoragua.py
+# virtualenv is now active, which means your PATH has been modified.
+# Don't try to run python from /usr/bin/python, just run "python" and
+# let the PATH figure out which version to run (based on what your
