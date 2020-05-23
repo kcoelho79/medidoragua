2d1546a957f77e385ae951725264c3ab0d6abe39
Medidor de Agua <kleber.amg79@gmail.com>
2020-05-23 15:07:21 -0300

    create def open display headless

--- watermeter.py
+++ watermeter.py
@@ -1,9 +1,30 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
 class Watermeter:
     
     def __init__(self):
         self.url = 'https://datastudio.google.com/reporting/188wX_8wKVwiG8VBhAGheljpcqU18Dov1/page/bCkF'
+        self.data_collected = []
 
-    def fetch_data(self,url=None):
+        # Open HeadLess chromedriver
+    def start_display(self):
+        print("... starting display ...")
+        self.display = Display(visible=0, size=(800, 600))
+        self.display.start()
+        print("... started ... ")
+        
+    def close_display(self):
+        print('... closing display ...')
+        self.display.stop()
+        print('... closed!...')
+
+
+    def get_page(self,url=None):
         if url:
             self.url = url
 
