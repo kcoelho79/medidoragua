c1eeaf50fc94b69330f24ef917a60925afb44188
Medidor de Agua <kleber.amg79@gmail.com>
2020-05-23 12:15:40 -0300

    teste dash, plot

--- grafico.py
+++ grafico.py
@@ -3,43 +3,69 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
+from flask import Flask, Response
+import io, random
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 
-csv = pd.read_csv('data.csv', index_col= False , parse_dates = ["data"])
-csv['medicao'] = pd.to_numeric(csv['medicao'].str.split(".").str.get(0))
-csv['horas'] = pd.to_datetime(csv['horas'], format='%H:%M').dt.time
-print(csv.dtypes)
-print(csv['horas'])
-#fig = px.line(csv, x="horas", y="medicao")
-#fig.show()
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+def preparaDados():
+    csv = pd.read_csv('data.csv', index_col= False , parse_dates = ["data"])
+    csv['medicao'] = pd.to_numeric(csv['medicao'].str.split(".").str.get(0))
+    csv['horas'] = pd.to_datetime(csv['horas'], format='%H:%M').dt.time
+    return csv
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = Flask(__name__)
 
+@app.route('/plot.png')
+def plot_png():
+    csv = preparaDados()
+    fig = create_figure(csv)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-app.layout = html.Div(children=[
-    html.H1(children='Caixa 119'),
+def create_figure(csv):
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = csv['dia'] 
+    ys = csv['medicao']
+    axis.plot(xs, ys)
+    return fig
 
-    html.Div(children='''
-        Medidor de Agua: Leitor da Caixa de Água .
-    '''),
+def grafico_dash(csv):
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': csv['horas'], 'y': csv['medicao'], 'type': 'line', 'name': 'SF'},
-                
-            ],
-            'layout': {
-                'title': 'Mediçao Caixa de Agua'
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+    app.layout = html.Div(children=[
+        html.H1(children='Caixa 119'),
+
+        html.Div(children='''
+            Medidor de Agua: Leitor da Caixa de Água .
+        '''),
+
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': [
+                    {'x': csv['horas'], 'y': csv['medicao'], 'type': 'line', 'name': 'SF'},
+                    
+                ],
+                'layout': {
+                    'title': 'Mediçao Caixa de Agua'
+                }
             }
-        }
-    )
-])
+        )
+    ])
+
+
+
+# grafico_dash(csv)
+
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)
 
    
