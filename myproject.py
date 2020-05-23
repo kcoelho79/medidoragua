35683b6fa4fedb7679bb64bb0e0ebda33b273bbc
Medidor de Agua <kleber.amg79@gmail.com>
2020-05-23 12:21:15 -0300

    update myprojecy.py

--- myproject.py
+++ myproject.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from flask import Flask
+import pandas as pd
 import plotly.express as px
 
 
@@ -7,22 +7,21 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-
-
 @app.route("/")
 def index():
-    return '<h1>--- DEU CERTO -ˆNN-  </h1>'
+    return '<h1> Exemplo Rodando APP Flask Nginx e uwsgi </h1>'
 
+
+@app.route("/grafico")
 def grafico():
-
     csv = pd.read_csv('data.csv', index_col= False , parse_dates = ["data"])
     csv['medicao'] = pd.to_numeric(csv['medicao'].str.split(".").str.get(0))
+    csv['horas'] = pd.to_datetime(csv['horas'], format='%H:%M').dt.time
     print(csv.dtypes)
+    print(csv['horas'])
     fig = px.line(csv, x="horas", y="medicao")
     fig.show()
-
-
-
+    
 
 
 if __name__ == "__main__":
